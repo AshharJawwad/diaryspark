c41dd9e6f10f9fc9903f1e7b343cce79cb4a3c19
Ashhar Jawwad <rockashhar10@gmail.com>
2026-08-10 22:24:05 +0530

    Made the Navbar Fully Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,43 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
-    <nav className="flex fixed h-16 w-full justify-around items-center px-5 bg-amber-400 m-0">
+    <nav className="flex fixed h-16 w-full justify-between pl-2 bg-white/70 backdrop-blur-xs border-b lg:border-none md:bg-white/70 md:backdrop-blur-xs lg:backdrop-blur-none lg:justify-around items-center sm:px-2 sm:z-40 lg:px-5 lg:z-30 lg:bg-transparent">
       {/* Logo Area */}
-      <div className="text-center bg-white/70 backdrop-blur-md p-2 rounded-full w-44">
+      <div className="text-center lg:bg-white/70 lg:backdrop-blur-xs lg:p-2 lg:rounded-full lg:w-44">
         <h2 className="text-2xl font-extrabold font-mono">DiarySpark</h2>
       </div>
 
       {/* Navigation Area */}
-      <div className="hidden lg:block items-center justify-center bg-white/70 backdrop-blur-md p-2 rounded-full">
-        <Link href="/" className="mx-3 font-mono text-lg cursor-pointer">
+      <div className="hidden sm:bg-none sm:backdrop-blur-none lg:flex lg:items-center lg:justify-center lg:bg-white/70 lg:backdrop-blur-xs lg:p-2 lg:rounded-full">
+        <Link
+          href="/"
+          className="mx-3 font-mono font-semibold text-lg cursor-pointer"
+        >
           Home
         </Link>
-        <Link href="/blog" className="mx-3 font-mono text-lg cursor-pointer">
+        <Link
+          href="/blog"
+          className="mx-3 font-mono font-semibold text-lg cursor-pointer"
+        >
           Blog
         </Link>
-        <Link href="/quiz" className="mx-3 font-mono text-lg cursor-pointer">
+        <Link
+          href="/quiz"
+          className="mx-3 font-mono font-semibold text-lg cursor-pointer"
+        >
           Quiz
         </Link>
         <Link
           href="/challenge"
-          className="mx-3 font-mono text-lg cursor-pointer"
+          className="mx-3 font-mono font-semibold text-lg cursor-pointer"
         >
           Challenge
         </Link>
       </div>
 
       {/* Login Button & Mobile Navigation Area */}
-      <div className="flex justify-center p-2 bg-white/70 backdrop-blur-md rounded-full min-w-38">
-        <Button className="hidden md:block bg-primary font-mono text-sm cursor-pointer">
+      <div className="flex justify-center px-0 lg:p-2 lg:bg-white/70 lg:backdrop-blur-xs lg:rounded-full min-w-38">
+        <Button className="bg-primary font-mono text-sm cursor-pointer">
           Login/Register
         </Button>
       </div>
